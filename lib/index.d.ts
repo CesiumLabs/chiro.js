88abf3f3f1d3794e18c52a7ff9b059d0066866d9
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2021-08-02 15:48:28 +0530

    fix: Removed lib/* files.

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { Manager } from "./structures/Manager";
-export { Player } from "./structures/Player";
-export { Node } from "./structures/Node";
-export { Queue } from "./structures/Queue";
-export * from "./Static/Interfaces";
-export * from "./Static/Constants";
--- lib/structures/Utils.d.ts
+++
@@ -1,4 +0,0 @@
-import { Extendable } from "../Static/Interfaces";
-export declare abstract class Structure {
-    static get<K extends keyof Extendable>(name: K): Extendable[K];
-}
